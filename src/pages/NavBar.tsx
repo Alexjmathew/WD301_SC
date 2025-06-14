6c5c8bcc80c5b55cd59fd5ea4bb851b03e9b1658
Alex J Mathew <131454666+Alexjmathew@users.noreply.github.com>
2025-06-14 19:16:20 +0530

    Update NavBar.tsx

--- src/pages/NavBar.tsx
+++ src/pages/NavBar.tsx
@@ -17,13 +17,12 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className="bg-gray-800 px-2 py-2">
+    <nav className="bg-blue-900 px-2 py-2">
       <div className="flex justify-between items-center">
         <div className='flex flex-wrap justify-center items-center'>
           <Link to="/" className="flex items-center text-white text-3xl font-bold px-2">
             Sports Live
           </Link>
-          {/* <p className="text-white text-xl p-1 italic p-1">Step into the Arena</p> */}
         </div>
         <div className="flex flex-wrap space-x-4 text-white text-lg items-center">
           {isLoggedIn && (
@@ -45,10 +44,10 @@ const Navbar: React.FC = () => {
                 onBlur={() => setDropdownOpen(false)}
                 className={`text-white hover:bg-gray-700 px-3 py-2 rounded-md focus:outline-none group-hover:bg-gray-700 ${isDropdownOpen ? 'focus:ring focus:ring-gray-400' : ''}`}
               >
-                <span>&#x2314;</span>
+                <span>⌔</span>
               </button>
               <div
-                className={`origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none transition-all duration-300 ${isDropdownOpen ? 'opacity-100 scale-100' : 'opacity-0 scale-95'}`}
+                className={`origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-gray-50 ring-1 ring-black ring-opacity-5 focus:outline-none transition-all duration-300 ${isDropdownOpen ? 'opacity-100 scale-100' : 'opacity-0 scale-95'}`}
               >
                 <div className="py-1 font-semibold">
                   <Link
